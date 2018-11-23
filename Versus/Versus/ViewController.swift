c27bfd8f2ae3c683401fdcde7b603d44ade4b881
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-11-23 09:14:04 +0100

    Init project :zap:

---
+++ Versus/Versus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Versus
+//
+//  Created by Matthieu T on 23/11/2018.
+//  Copyright © 2018 MT Creative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
