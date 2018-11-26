26237cf6d85b44b854a593d5b7641da9a5d30602
Matthieu Toussaint <matthieu.toussaint@hetic.net>
2018-11-26 19:56:39 +0100

    Home view controler with links

--- Versus/Versus/HomeViewController.swift
+++ Versus/Versus/HomeViewController.swift
@@ -12,6 +12,18 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var labelTournament: UILabel!
     @IBOutlet weak var textFieldTournament: UITextField!
+    @IBOutlet weak var labelGame: UILabel!
+    @IBOutlet weak var textFieldGame: UITextField!
+    @IBOutlet weak var labelPlayers: UILabel!
+    @IBOutlet weak var playersPickerView: UIPickerView!
+    @IBOutlet weak var textFieldPlayers: UITextField!
+    @IBOutlet weak var labelPlayerOne: UILabel!
+    @IBOutlet weak var textFieldPlayerOne: UITextField!
+    @IBOutlet weak var labelPlayerTwo: UILabel!
+    @IBOutlet weak var textFieldPlayerTwo: UITextField!
+    @IBOutlet weak var buttonLetsRumble: UIButton!
+    
+    let playersNumber = [1,2,3,4,5,6,7,8]
     
     override func viewDidLoad() {
         super.viewDidLoad()
